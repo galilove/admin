dd5a489cf475db4db5f8f1766b4271e805a45ad5
lijiali <lijiali@baoyinxiaofei.com>
2021-01-10 17:53:33 +0800

    修改security模块

---
+++ sany-security/src/main/java/com/sany/security/SanySecurityApplication.java
@@ -0,0 +1,17 @@
+package com.sany.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Author lijiali
+ * @Description //权限服务主启动类
+ * @Date 下午 2:32 2021/1/10 0010
+ **/
+@SpringBootApplication
+public class SanySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanySecurityApplication.class, args);
+    }
+
+}
---
+++ sany-security/src/test/java/com/sany/security/EladminSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sany.security;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EladminSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
